1de22dfca61656367dbc39cd03c8b5a3b5f3b90f
kkpol1 <konradpietapl@gmail.com>
2017-05-18 10:57:28 +0200

    change

--- first_game.py
+++ first_game.py
@@ -6,28 +6,7 @@ from game_inventory import *
 from game_menu import *
 from hotncold import *
 from riddles import *
-
-
-class colours:
-    Blue = '\033[94m'
-    Green = '\033[92m'
-    Yellow = '\033[93m'
-    Barier = '\033[0m'
-    Bold = '\033[1m'
-    Underline = '\033[4m'
-    Red = '\033[31m'
-
-
-class background:
-    black = '\033[40m'
-    red = '\033[41m'
-    green = '\033[42m'
-    orange = '\033[43m'
-    blue = '\033[44m'
-    purple = '\033[45m'
-    cyan = '\033[46m'
-    lightgrey = '\033[47m'
-
+from color import *
 
 def create_board(filename='stage1.txt'):
 
